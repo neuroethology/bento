c055dc56b4ea0a1dabe3420d330883acbc65b140
drumph <drumph@caltech.edu>
2021-09-28 13:59:20 -0700

    Fix issue 29 - multiple copies of investigator

--- src/db/investigatorDialog.py
+++ src/db/investigatorDialog.py
@@ -24,18 +24,28 @@ class InvestigatorDialog(QDialog):
     def populateComboBox(self, preSelect):
         if preSelect:
             selection = self.ui.investigatorComboBox.currentText()
+            selection_username = None
         self.ui.investigatorComboBox.clear()
-        self.ui.investigatorComboBox.addItem("new Investigator")
+        # For some unknown reason, addItem resets self.investigator_id to None!
+        # So we need to preserve and restore it
+        investigator_id = self.investigator_id
+        self.ui.investigatorComboBox.addItem("New Investigator")
+        self.investigator_id = investigator_id
         with self.bento.db_sessionMaker() as db_sess:
             try:
                 investigators = db_sess.query(Investigator).distinct().all()
                 self.ui.investigatorComboBox.addItems([elem.user_name for elem in investigators])
+                if preSelect and self.investigator_id:
+                    selection_username = investigators[self.investigator_id-1].user_name
             except Exception as e:
                 pass # no database yet?
 
         self.ui.investigatorComboBox.setEditable(False)
         if preSelect:
-            self.ui.investigatorComboBox.setCurrentText(selection)
+            if self.investigator_id and self.investigator_id > 0 and selection_username == selection:
+                self.ui.investigatorComboBox.setCurrentIndex(self.investigator_id)
+            else:
+                self.ui.investigatorComboBox.setCurrentText(selection)
 
     @Slot(object)
     def update(self, button, preSelect=True):
@@ -43,14 +53,34 @@ class InvestigatorDialog(QDialog):
         if (buttonRole == QDialogButtonBox.AcceptRole or
             buttonRole == QDialogButtonBox.ApplyRole):
             with self.bento.db_sessionMaker() as db_sess:
-                investigator = Investigator()
-                investigator.user_name = self.ui.usernameLineEdit.text()
-                investigator.last_name = self.ui.lastNameLineEdit.text()
-                investigator.first_name = self.ui.firstNameLineEdit.text()
-                investigator.institution = self.ui.institutionLineEdit.text()
-                investigator.e_mail = self.ui.eMailLineEdit.text()
-                db_sess.add(investigator)
-                db_sess.commit()
+                need_to_add = False
+                data_changed = False
+                investigator = None
+                if self.investigator_id:
+                    investigator = db_sess.query(Investigator).filter(Investigator.id == self.investigator_id).scalar()
+                if not investigator:
+                    need_to_add = True
+                    investigator = Investigator()
+                if investigator.user_name != self.ui.usernameLineEdit.text():
+                    investigator.user_name = self.ui.usernameLineEdit.text()
+                    data_changed = True
+                if investigator.last_name != self.ui.lastNameLineEdit.text():
+                    investigator.last_name = self.ui.lastNameLineEdit.text()
+                    data_changed = True
+                if investigator.first_name != self.ui.firstNameLineEdit.text():
+                    investigator.first_name = self.ui.firstNameLineEdit.text()
+                    data_changed = True
+                if investigator.institution != self.ui.institutionLineEdit.text():
+                    investigator.institution = self.ui.institutionLineEdit.text()
+                    data_changed = True
+                if investigator.e_mail != self.ui.eMailLineEdit.text():
+                    investigator.e_mail = self.ui.eMailLineEdit.text()
+                    data_changed = True
+                if need_to_add:
+                    db_sess.add(investigator)
+                    data_changed = True
+                if data_changed:
+                    db_sess.commit()
             self.populateComboBox(preSelect)
         elif buttonRole == QDialogButtonBox.DestructiveRole:
             self.reject()
@@ -71,12 +101,11 @@ class InvestigatorDialog(QDialog):
         if self.ui.investigatorComboBox.currentIndex() == 0:
             self.investigator_id = None
         else:
-            investigator_username = self.ui.investigatorComboBox.currentText()
             with self.bento.db_sessionMaker() as db_sess:
-                query = db_sess.query(Investigator)
-                result = query.filter(Investigator.user_name == investigator_username).all()
-                if len(result) > 0:
-                    investigator = result[0]
+                investigators = db_sess.query(Investigator).all()
+                # -1 in the next two lines due to "New Investigator" first entry in combo box
+                if len(investigators) >= self.ui.investigatorComboBox.count()-1:
+                    investigator = investigators[self.ui.investigatorComboBox.currentIndex()-1]
                     self.investigator_id = investigator.id
                     self.ui.usernameLineEdit.setText(investigator.user_name)
                     self.ui.lastNameLineEdit.setText(investigator.last_name)
